9d11d8d130aff889271c11858f5e2880d4cfb1be
KDaria <dkdva@mail.ru>
2024-04-02 17:59:28 +0200

    added Realm

---
+++ TaskList&Realm/Factories/AlertControllerBuilder.swift
@@ -0,0 +1,8 @@
+//
+//  AlertControllerBuilder.swift
+//  TaskList&Realm
+//
+//  Created by Дарья Кобелева on 02.04.2024.
+//
+
+import Foundation
---
+++ TaskList&Realm/Services/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  TaskList&Realm
+//
+//  Created by Дарья Кобелева on 02.04.2024.
+//
+
+import Foundation
